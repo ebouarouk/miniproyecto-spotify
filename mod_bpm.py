f1cb10f96b63bd82a7da1c1f1933b96528aff703
Elise Louma Bouarouk <elise.bouarouk@gmail.com>
2022-11-03 19:23:13 +0100

    Added exception management and deleted folder_path variable

--- mod_bpm.py
+++ mod_bpm.py
@@ -1,32 +1,62 @@
 import sys
-
+from os.path import exists
 from spotify import Song
 
 
-'''Information about headers in datafile
+'''
+This program modifies the beat per minute (bpm) of songs in <input_file>:
+
+Call to program
+python3 mod_bpm.py <relative_bpm>
+    
+Input
+    <relative_bpm>: integer
+    top50.csv: datafile
+    
+Output
+    top50_mod.csv: datafile
+
+Information about headers in datafile
 0: track name
 1: artist name
 2: genre
 3: beats per minute
 4: energy
 5: danceability
-6: length'''
+6: length
 
-# poner el path hacia la carpeta de Maryam 
-folder_path = '/home/ebouarouk/miniproyecto-spotify/'
-# quitar esto y llamar al path de la carpeta
+Details are described at https://aprendepython.es/miniprojects/spotify/.
+Designed by Sergio Delgado Quintero.
+
+Developed by:
+Elise Louma Bouarouk
+Maryam Barra Bouchemla
+'''
 
 if __name__ == '__main__':
-    input_file = 'top50.csv'
+    # check top50.csv exists
+    if exists('top50.csv'):
+        input_file = 'top50.csv'
+    else:
+        print('The input file top50.csv does not exist in the current folder')
     output_file = 'top50_mod.csv'
-    relative_bpm = int(sys.argv[1])  # read keyboard input
-
+    
+    # read keyboard input
+    try:
+        relative_bpm = int(sys.argv[1])
+    except Exception:
+        print('Error: Integer expected as argument...')
+        
     # load songs (from input_file)
-    songs = Song.load_songs(folder_path + input_file)
+    songs = Song.load_songs(input_file)
     
     # change speed of all songs
     for song in songs:
         Song.change_speed(song, relative_bpm)
-
+   
     # save songs (to output_file)
-    Song.save_songs(songs, folder_path + output_file)
+    try:
+        Song.save_songs(songs, output_file)
+        print(f'Modified songs have been successfully stored in {output_file}')
+    except Exception:
+        print('Ups. Something went wrong.')
--- spotify.py
+++ spotify.py
@@ -10,31 +10,31 @@ class Song:
         self.length = length
 
     def __str__(self) -> str:
+        'Return a string of attributes separated by comma'
         return f"{self.track},{self.artist},{self.genre},{self.bpm},{self.energy},{self.danceability},{self.length}"
-        # need to shorten string without breaking format
 
     def change_speed(self, relative_bpm: int) -> None:
+        'Modify the beat per minute of the Song instance and update attributes'
         self.bpm += relative_bpm
         self.energy += 2 * relative_bpm
         self.danceability += 3 * relative_bpm
         self.length -= relative_bpm
-        # necesita comprobar resultado con relative_bpm negativo
         
     @staticmethod
     def load_songs(path: str) -> list:
+        'Return a list of Song instances created for each line of the file'
         f = open(path)
         songs = []
         for line in f:
-            song = line.split(',') # convert string line to list of str
-            [song.append(int(element)) for element in song[3::]] # convert to int and append  
-            song = song[0:3] + song[7::] # remove str duplicates
-            songs.append(Song(*song)) # create instances and append to list
+            song = line.split(',')
+            [song.append(int(element)) for element in song[3::]] # convert int
+            song = song[0:3] + song[7::]
+            songs.append(Song(*song)) # create Song instance
         return songs
-    # optimizar el codigo, hacerlo más guapo
 
     @staticmethod
     def save_songs(songs: list, path: str) -> None:
-        # if folder exists
+        'Create a datafile of Song instances at path'
         f = open(path, 'w')
         for song in songs:
             f.write(Song.__str__(song) + '\n')
